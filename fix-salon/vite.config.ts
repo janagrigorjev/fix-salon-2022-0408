be08e192138887bc3700992975fd51a26f7aa6ff
janagrigorjev <lucija000111@gmail.com>
2024-09-15 00:11:16 +0200

    Testing

---
+++ fix-salon/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { configDefaults } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+  },
+});
